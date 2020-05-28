87e99701268d8cb11530b3242ff2c4c82571c0b8
AvvALl <vlad.arefev.00@mail.ru>
2020-05-29 01:07:42 +0300

    Update Tests.cs

--- GroupTaskTests/Tests.cs
+++ GroupTaskTests/Tests.cs
@@ -9,7 +9,29 @@ namespace GroupTaskTests
     {
        
 
-     
+        [TestMethod]
+        public void TestNormalizeFrobenius()
+        {
+            var a = new double[,] {
+                { 5.5, 1},
+                { 44, 30},
+                { 0, 5}
+            };
+
+            double[,] expected = new double[,]
+            {
+                {0.1, 0.02},
+                {0.82, 0.56},
+                {0, 0.09}
+            };
+
+            var res = Operations.NormalizeFrobenius(a);
+            for (int i = 0; i < res.GetLength(0); i++)
+                for (int j = 0; j < res.GetLength(1); j++)
+                    res[i, j] = Math.Round(res[i, j], 2);
+
+            CollectionAssert.AreEqual(res, expected);
+        }
 
         [TestMethod]
         public void TestLowerTriangular()
